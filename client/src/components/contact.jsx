3a9157a3f1eb6bf93d392e5aee5743164e097972
Rui Dantas <ruidantas55@gmail.com>
2023-06-09 11:37:19 -0400

    Download resume, Add contact functionality, and some css changes

--- client/src/components/contact.jsx
+++ client/src/components/contact.jsx
@@ -1,20 +1,58 @@
-import React from "react";
-import "../style/Contact.scss"
+import React, { useRef, useState, useEffect } from "react";
 
-
-
-
+import emailjs from "@emailjs/browser";
+import "../style/Contact.scss";
+import { RiH1 } from "react-icons/ri";
 
 
 export default function Contact() {
+  const [emailSent, setIsEmailSent] = useState(false);
+  const form = useRef();
+
+  useEffect(() => {
+    setIsEmailSent(false);
+  },[])
+
+  const setEmail = () => {
+    setTimeout(() => {
+      setIsEmailSent(false);
+    }, 5000);
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_f8ra68x",
+        "template_kdq4osh",
+        form.current,
+        "arf-Oa_lc-bX5uylp"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setIsEmailSent(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact-container">
-        <input type="text" placeholder="First Name"/>
-        <input type="text" placeholder="Last Name"/>
-        <input type="text" placeholder="Email"/>
-        <textarea placeholder="Write something..."></textarea>
-        <button onClick={() => console.log("Hello")}>Send</button>
-        
+      {!emailSent && (
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" placeholder="Name" name="user_name" />
+          <input type="email" placeholder="Email" name="user_email" />
+          <textarea placeholder="Write something..." name="message"></textarea>
+          <button type="submit" value="Send" onClick={() => { setEmail() }}>
+            Send
+          </button>
+        </form>
+      )}
+      {emailSent && <span className="email--sent">Your email has been sent!</span>}
     </div>
   );
 }
--- client/src/components/info.jsx
+++ client/src/components/info.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { AiOutlineLinkedin, AiFillGithub, AiOutlineInstagram } from "react-icons/ai"
+import { RiFileDownloadLine } from "react-icons/ri"
 import "../style/Info.scss"
+
 
 export default function Info() {
   return (
     <div className="social--media">
       <a href="https://www.linkedin.com/in/rui-dantas-043078248/" target="#" className="links"><AiOutlineLinkedin />LinkedIn</a> 
       <a href="https://github.com/Ruheee" target="#" className="links"><AiFillGithub />Github</a> 
-      <a href="https://instagram.com/Ruixalexandxr" target="#" className="links"><AiOutlineInstagram />Instagram</a> 
+      <a href="https://instagram.com/Ruixalexandxr" target="#" className="links"><AiOutlineInstagram />Instagram</a>
+      <a download="Ruis_Resume.pdf "target="_blank" href="Ruis_Resume.pdf" className="links"><RiFileDownloadLine />Resume</a>  
+      
     </div>
   );
 }
